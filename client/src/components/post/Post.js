07b8808fb941e9b12b265b5fec5de6946635121e
fad16papa <fad16papa@gmail.com>
2020-08-13 00:18:52 +0800

    Commi changes

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -18,7 +18,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
   ) : (
     <Fragment>
       <Link to="/posts" className="btn">
-        Back To Posts
+        Back To Post
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
