475061a54949b0c7c0471f40200f4482b9a1ce5d
fad16papa <fad16papa@gmail.com>
2020-08-13 10:23:30 +0800

    Commit for deployment

---
+++ client/build/precache-manifest.3d35ab18171e1552abfa752406cb4bda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8af92576a81fe64f66ff79d977163f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "062ca38ba870bf443b0f",
+    "url": "/static/css/main.7d9d1bb1.chunk.css"
+  },
+  {
+    "revision": "278cc1db19d903dcf6e2",
+    "url": "/static/js/2.9157e84e.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.9157e84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062ca38ba870bf443b0f",
+    "url": "/static/js/main.8834f180.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
